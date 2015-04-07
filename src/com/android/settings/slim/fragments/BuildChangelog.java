9b70555abcfa3b6fb82269c0ea6662e3e392b97b
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-04-08 00:30:41 +0200

    AdvancedSettings: Build Changelog
    Change-Id: I707e1d79f7580a68e819b8efdcea4a1ab47839bf

---
+++ src/com/android/settings/slim/fragments/BuildChangelog.java
@@ -0,0 +1,47 @@
+package com.android.settings.slim.fragment;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.slim.util.Changelog;
+
+public class BuildChangelog extends SettingsPreferenceFragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.build_changelog, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Changelog rc = new Changelog() {
+            @Override
+            public void onResponseReceived(String result) {
+                final TextView changelogTextView = (TextView) getView().findViewById(R.id.tv_changelog);
+                if (changelogTextView == null) {
+                    return;
+                }
+                changelogTextView.setText(result);
+            }
+        };
+        rc.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, getActivity());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
